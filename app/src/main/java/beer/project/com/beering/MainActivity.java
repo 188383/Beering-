842409a48817107dd7250808d46fe8ac573dd36d
188383 <188383@student.pwr.edu.pl>
2015-05-19 19:13:12 +0200

    options: username mail(identifier)
    connection options: ip address of server and port

--- app/src/main/java/beer/project/com/beering/MainActivity.java
+++ app/src/main/java/beer/project/com/beering/MainActivity.java
@@ -9,10 +9,19 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import org.json.*;
 
-public class MainActivity extends Activity {
 
+public class MainActivity extends Activity{
 
+    private String mail;
+    private String username;
+
+    public static final String ADDRESS = "beer.project.com.beering.ADDRESS";
+    public static final String PORT = "beer.project.com.beering.PORT";
+
+    private String hostAddress;
+    private int hostPort;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,11 +29,21 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
 
         Button listBeers = (Button)findViewById(R.id.list_beers);
-
+        //activates the list of beers
         listBeers.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(),BeerList.class);
+                startActivity(intent);
+            }
+        });
+
+        Button optionsButton = (Button)findViewById(R.id.options);
+
+        optionsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),OptionsActivity.class);
                 startActivity(intent);
             }
         });
--- app/src/main/java/beer/project/com/beering/OptionsActivity.java
+++ app/src/main/java/beer/project/com/beering/OptionsActivity.java
@@ -1,29 +1,17 @@
 package beer.project.com.beering;
 
 import android.app.Activity;
-import android.app.FragmentManager;
-import android.app.SearchManager;
-import android.content.Context;
-import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.TextView;
-import android.widget.Toast;
 
 
-
-public class BeerList extends Activity {
+public class OptionsActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.beer_list);
-
+        setContentView(R.layout.options_layout);
     }
-
-
-
 }
