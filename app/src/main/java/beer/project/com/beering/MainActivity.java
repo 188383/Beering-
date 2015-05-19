008ce208a8c23b4420527891010c6db84aef8986
188383 <188383@student.pwr.edu.pl>
2015-05-19 14:15:23 +0200

    added landscape

--- app/src/main/java/beer/project/com/beering/MainActivity.java
+++ app/src/main/java/beer/project/com/beering/MainActivity.java
@@ -1,18 +1,34 @@
 package beer.project.com.beering;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends Activity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button listBeers = (Button)findViewById(R.id.list_beers);
+
+        listBeers.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),BeerList.class);
+                startActivity(intent);
+            }
+        });
+
     }
 
 
